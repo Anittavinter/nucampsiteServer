d1d646e756ac9123991a89acd7f25200ce4cc383
Anittavinter <anittavinter@gmail.com>
2025-02-27 20:25:31 +0100

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,9 +32,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Removed cookie parser and using sessions instead
-// app.use(cookieParser('12345-67890-09876-54321'));
-
 app.use(session({
   name: 'session-id',
   secret: '12345-67890-09876-54321',
@@ -41,31 +40,26 @@ app.use(session({
   store: new FileStore()
 }));
 
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     const err = new Error('You are not authenticated!');
     err.status = 401;
     return next(err);
   } else {
-    if (req.session.user === 'authenticated') {
-      return next();
-    } else {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 }
 
 app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
