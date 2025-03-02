12dae7ae7b395d1bb8fce3789398f9937fcde188
Anittavinter <anittavinter@gmail.com>
2025-03-02 16:01:09 +0100

    Mongoose Population

--- models/campsite.js
+++ models/campsite.js
@@ -15,8 +15,8 @@ const commentSchema = new Schema({
         required: true
     },
     author: {
-        type: String,
-        required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
@@ -55,4 +55,5 @@ const campsiteSchema = new Schema({
 });
 
 const Campsite = mongoose.model('Campsite', campsiteSchema);
+
 module.exports = Campsite;
